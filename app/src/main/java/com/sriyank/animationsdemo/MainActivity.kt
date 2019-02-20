b1085b7611784460ed159602bb7fe2fed71e611c
Smartherd <smartherd@gmail.com>
2019-02-20 20:51:41 +0530

    module 3 property animation final code

--- app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
+++ app/src/main/java/com/sriyank/animationsdemo/MainActivity.kt
@@ -1,40 +1,106 @@
 package com.sriyank.animationsdemo
 
-import android.graphics.drawable.AnimatedVectorDrawable
+import android.animation.Animator
+import android.animation.ObjectAnimator
+import android.animation.ValueAnimator
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Animator.AnimatorListener {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+	private var rotateAnimator: ObjectAnimator? = null
+	private var translateAnimator: ObjectAnimator? = null
+	private var scaleAnimator: ObjectAnimator? = null
+	private var fadeAnimator: ObjectAnimator? = null
 
-        var isChecked = true
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_main)
 
-        avdImage.setOnClickListener {
-            if (isChecked)
-                checkToClose()
-            else
-                closeToCheck()
+	}
 
-            isChecked = !isChecked
-        }
-    }
+	fun rotateAnimation(view: View) {
 
-    private fun closeToCheck() {
+		rotateAnimator = ObjectAnimator.ofFloat(targetImage, "rotation", 0.0f, -180.0f)
+		rotateAnimator?.apply {
+			duration = 1000
+			repeatCount = 1
+			repeatMode = ValueAnimator.REVERSE
+			addListener(this@MainActivity)
+			start()
+		}
+	}
 
-        avdImage.setImageResource(R.drawable.avd_close_to_check)
-        val avdCheckToClose = avdImage.drawable as AnimatedVectorDrawable
-        avdCheckToClose.start()
-    }
+	fun scaleAnimation(view: View) {
 
-    private fun checkToClose() {
+		scaleAnimator = ObjectAnimator.ofFloat(targetImage, "scaleX", 1.0f, 3.0f)
+		scaleAnimator?.apply {
+			duration = 1000
+			repeatCount = 1
+			repeatMode = ValueAnimator.REVERSE
+			addListener(this@MainActivity)
+			start()
+		}
+	}
 
-        avdImage.setImageResource(R.drawable.avd_check_to_close)
-        val avdCheckToClose = avdImage.drawable as AnimatedVectorDrawable
-        avdCheckToClose.start()
-    }
+	fun translateAnimation(view: View) {
+
+		translateAnimator = ObjectAnimator.ofFloat(targetImage, "translationX", 0f, 200f)
+		translateAnimator?.apply {
+			duration = 1000
+			repeatCount = 1
+			repeatMode = ValueAnimator.REVERSE
+			addListener(this@MainActivity)
+			start()
+		}
+
+		// translateAnimator.cancel()
+	}
+
+	fun fadeAnimation(view: View) {
+
+		fadeAnimator = ObjectAnimator.ofFloat(targetImage, "alpha", 1.0f, 0.0f)
+		fadeAnimator?.apply {
+			duration = 1500
+			repeatCount = 1
+			repeatMode = ValueAnimator.REVERSE
+			addListener(this@MainActivity)
+			start()
+		}
+	}
+
+	// Implementation of AnimatorListener interface
+	override fun onAnimationStart(animation: Animator?) {
+
+		if (animation == scaleAnimator)
+			Toast.makeText(this, "Scale Animation Started", Toast.LENGTH_SHORT).show()
+
+		if (animation == rotateAnimator)
+			Toast.makeText(this, "Rotate Animation Started", Toast.LENGTH_SHORT).show()
+
+		if (animation == translateAnimator)
+			Toast.makeText(this, "Translate Animation Started", Toast.LENGTH_SHORT).show()
+
+		if (animation == fadeAnimator)
+			Toast.makeText(this, "Fade Animation Started", Toast.LENGTH_SHORT).show()
+	}
+
+	override fun onAnimationRepeat(animation: Animator?) {
+
+		Toast.makeText(this, "Animation Repeated", Toast.LENGTH_SHORT).show()
+	}
+
+	override fun onAnimationEnd(animation: Animator?) {
+
+		Toast.makeText(this, "Animation Ended", Toast.LENGTH_SHORT).show()
+	}
+
+	override fun onAnimationCancel(animation: Animator?) {
+
+		Toast.makeText(this, "Animation Cancelled", Toast.LENGTH_SHORT).show()
+	}
 }
